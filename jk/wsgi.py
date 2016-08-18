87546fe09e7177f8b47ea78329f3a07ff77bb375
Feng Xia <feng_xia41103@hotmail.com>
2016-08-17 21:00:33 -0400

    Update wsgi.py to make it compatible with test environment which uses uwsgi to handle virtualenv.

--- jk/wsgi.py
+++ jk/wsgi.py
@@ -1,19 +1,7 @@
 import os
-import sys
-import site
-
-# Add the site-packages of the chosen virtualenv to work with
-site.addsitedir('~/.virtualenvs/gaokao/local/lib/python2.7/site-packages')
-
-# Add the app's directory to the PYTHONPATH
-sys.path.append('/home/fengxia/workspace/jk')
-sys.path.append('/home/fengxia/workspace/jk/jk')
+import os.path
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'jk.settings'
-
-# Activate your virtual env
-activate_env = os.path.expanduser("~/.virtualenvs/gaokao/bin/activate_this.py")
-execfile(activate_env, dict(__file__=activate_env))
 
 # Fix django closing connection to MemCachier after every request (#11331)
 # http://blog.memcachier.com/2014/12/12/django-persistent-memcached-connections/
